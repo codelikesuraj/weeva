6c1f5653c9fd4a9c24230b699806514281d376eb
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-03-31 18:00:40 +0100

    CHANGE: changed project name (from wiva to weeva) and removed unneeded laravel breeze files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,63 +50,57 @@
 			<nav class="sb-sidenav accordion sb-sidenav-dark sb-sidenav-light" id="sidenavAccordion">
 				<div class="sb-sidenav-menu">
 					<div class="nav">
+						<!-- Section -->
 						<div class="sb-sidenav-menu-heading">Core</div>
-						<a class="nav-link" href="index.html">
+						<a class="nav-link" href="{{route('dashboard')}}">
 							<div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
 							Dashboard
 						</a>
-						<div class="sb-sidenav-menu-heading">Interface</div>
+
+						<!-- Section -->
+						<div class="sb-sidenav-menu-heading">Menu Heading</div>
 						<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
 							<div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-							Layouts
+							Menu 1
 							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 						</a>
 						<div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
 							<nav class="sb-sidenav-menu-nested nav">
-								<a class="nav-link" href="layout-static.html">Static Navigation</a>
-								<a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+								<a class="nav-link" href="#">Link 1</a>
+								<a class="nav-link" href="#">Link 2</a>
 							</nav>
 						</div>
 						<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
 							<div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-							Pages
+							Menu 2
 							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 						</a>
 						<div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
 							<nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
 								<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-									Authentication
+									Sub-menu 1
 									<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 								</a>
 								<div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
 									<nav class="sb-sidenav-menu-nested nav">
-										<a class="nav-link" href="login.html">Login</a>
-										<a class="nav-link" href="register.html">Register</a>
-										<a class="nav-link" href="password.html">Forgot Password</a>
+										<a class="nav-link" href="#">Link 1</a>
+										<a class="nav-link" href="#">Link 2</a>
+										<a class="nav-link" href="#">Link 3</a>
 									</nav>
 								</div>
 								<a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-									Error
+									Sub-menu 2
 									<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 								</a>
 								<div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
 									<nav class="sb-sidenav-menu-nested nav">
-										<a class="nav-link" href="401.html">401 Page</a>
-										<a class="nav-link" href="404.html">404 Page</a>
-										<a class="nav-link" href="500.html">500 Page</a>
+										<a class="nav-link" href="#">Link 1</a>
+										<a class="nav-link" href="#">Link 2</a>
+										<a class="nav-link" href="#">Link 3</a>
 									</nav>
 								</div>
 							</nav>
 						</div>
-						<div class="sb-sidenav-menu-heading">Addons</div>
-						<a class="nav-link" href="charts.html">
-							<div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-							Charts
-						</a>
-						<a class="nav-link" href="tables.html">
-							<div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-							Tables
-						</a>
 					</div>
 				</div>
 				<div class="sb-sidenav-footer">
