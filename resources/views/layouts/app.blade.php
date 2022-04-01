e4a6726fc2cbc47379c77a6095d9c3ee578fde03
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-04-01 14:03:08 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="" />
-	<meta name="author" content="" />
+	<meta name="description" content="Order management software tailored for aso-oke weavers" />
+	<meta name="author" content="Weeva" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>{{$title}}</title>
