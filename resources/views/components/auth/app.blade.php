aa509407c8c41fed5b7e8608c985cc6eedfd36af
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-04-01 14:05:35 +0100

    Update app.blade.php

--- resources/views/components/auth/app.blade.php
+++ resources/views/components/auth/app.blade.php
@@ -4,8 +4,8 @@
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
+  <meta name="description" content="{{$title}}" />
+  <meta name="author" content="Weeva" />
   <title>{{$title}}</title>
   <link href="/assets/css/styles.css" rel="stylesheet" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
