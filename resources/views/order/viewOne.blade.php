8beaf8b7aea856d068a45f00e7607eb318528b29
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-04-03 19:07:02 +0100

    FIX: typo

--- resources/views/order/viewOne.blade.php
+++ resources/views/order/viewOne.blade.php
@@ -25,47 +25,47 @@
 			        Order Details
 			      </button>
 			    </h2>
-			    <div id="orderDetails" class="accordion-collapse collapse show p-0" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-			      <div class="accordion-body p-2">
-			      	<div class="row">
-			      		<div class="col-12">
-				        	<div class="row mx-0">
-				        		<div class="col-8 p-0">
-				        			<div class="col-12 mb-1 mx-0">
-					              <strong>Status:&nbsp;</strong>
+			    <div id="orderDetails" class="fs-6 accordion-collapse collapse show p-0" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+			      <div class="accordion-body p-2 fs-6">
+			      	<div class="row  fs-6">
+			      		<div class="col-12  fs-6">
+				        	<div class="row mx-0  fs-6">
+				        		<div class="col-8 p-0  fs-6">
+				        			<div class="col-12 mb-1 mx-0  fs-6">
+					              <strong class=" fs-6">Status:&nbsp;</strong>
 					              @if($order->status == 'pending')
-					              	<span class="bg-warning text-body px-1">
+					              	<span class="bg-warning text-body px-1  fs-6">
 					             	@else
-					             		<span class="bg-success text-white px-1">
+					             		<span class="bg-success text-body px-1  fs-6">
 					             	@endif
 					             		{{ucfirst($order->status)}}
 					             	</span>
 					            </div>
-				        			<div class="col-12 mb-1">
-				        				<strong>No:</strong>
+				        			<div class="col-12 mb-1  fs-6">
+				        				<strong class=" fs-6">No:</strong>
 				        				<a class="text-dark" href="{{route('waybillNo', [$order->waybill_no])}}"> {{$order->waybill_no}}</a>
 				        			</div>
 				        			<div class="col-12 mb-1">
-				        				<strong>Issued on:</strong> {{date('d-M-Y', strtotime($order->date_issued))}}
+				        				<strong class=" fs-6">Issued on:</strong> {{date('d-M-Y', strtotime($order->date_issued))}}
 				        			</div>
 				        		</div>
-				        		<div class="col-4 p-0 d-flex justify-content-end">
+				        		<div class=" fs-6 col-4 p-0 d-flex justify-content-end">
 				        			<div class="">
 				        				<a class="btn btn-success py-1" href="{{route('editOrder', [$order->id])}}">Edit</a>
 				        			</div>
 				        		</div>
 				        	</div>
 					      </div>
-			        	<div class="col-12 mb-1"><strong>Description:</strong>
-			        		<div class="my-0 p-2" style="background: #eeeeee;">
+			        	<div class="col-12 mb-1 fs-6"><strong class=" fs-6">Description:</strong>
+			        		<div class="my-0 p-2 fs-6" style="background: #eeeeee;">
 			        			{{$order->quantity.' '.$order->value.' of '.$order->description}}
 			        		</div>
 			        	</div>
-			        	<div class="col-12 mb-1"><strong>Issued By:</strong> 
-			        		<a class="text-dark" href="{{route('issuedBy', [$order->issued_by])}}">{{$order->issuedBy->name}}</a>
+			        	<div class="col-12 mb-1 fs-6"><strong class=" fs-6">Issued By:</strong> 
+			        		<a class="text-dark fs-6" href="{{route('issuedBy', [$order->issued_by])}}">{{$order->issuedBy->name}}</a>
 			        	</div>
-			        	<div class="col-12 mb-1"><strong>Customer:</strong> {{$order->customer_name}}</div>
-			        	<div class="col-12 mb-1"><strong>Deadline:</strong> {{$order->deadline != '' ? date('d-M-Y', strtotime($order->deadline)) : 'not specified'}}</div>
+			        	<div class="col-12 mb-1 fs-6"><strong class=" fs-6">Customer:</strong> {{$order->customer_name}}</div>
+			        	<div class="col-12 mb-1 fs-6"><strong class=" fs-6">Deadline:</strong> {{$order->deadline != '' ? date('d-M-Y', strtotime($order->deadline)) : 'not specified'}}</div>
 				      </div>
 			      </div>
 			    </div>
@@ -77,7 +77,7 @@
 			      </button>
 			    </h2>
 			    <div id="deliveryDetails" class="accordion-collapse collapse p-0" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-			      <div class="accordion-body p-2 text-center">
+			      <div class="accordion-body p-2 text-center fs-6">
 			        ⚠ Feature coming soon !!!
 			      </div>
 			    </div>
