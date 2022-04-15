4dd7c93a6c336b4c89ce666c38f71dd3bf7db61e
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-04-15 17:41:21 +0100

    FIX: fixed typo

--- resources/views/order/pending.blade.php
+++ resources/views/order/pending.blade.php
@@ -12,7 +12,7 @@
     
     <div class="card-header d-flex justify-content-between align-items-center">
       <div>
-        <h4 class="fs-4"><i class="fas fa-hourglass me-1"></i>Pending {{$orders->count()>0 ? $orders->count() : '0'}})</h4>
+        <h4 class="fs-4"><i class="fas fa-hourglass me-1"></i>Pending ({{$orders->count()>0 ? $orders->count() : '0'}})</h4>
       </div>
       <div>
         <a href="{{route('order')}}" class="btn btn-success p-1 px-2 small">Add</a>
